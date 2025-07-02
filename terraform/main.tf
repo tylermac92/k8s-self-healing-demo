2d8357c12a452e9559d670d595b0f1ae415e9969
tylermac92 <tylermac92@gmail.com>
2025-07-02 17:16:44 +0000

    fixing tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+    required_version = ">= 1.12.0"
+
+    required_providers {
+        google = {
+            source = "hashicorp/google"
+            version = ">= 6.42.0"
+        }
+    }
+}
+
 provider "google" {
     project = var.project_id
     region = var.region
